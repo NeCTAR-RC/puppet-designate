891b7145de119b21440ebcc6586874f6ae229c3b
Sam Morrison <sorrison@gmail.com>
2015-01-16 15:26:42 +1100

    Added class to use for designate development
    Change-Id: I624f03f1835794b35ba15702d4a3b62bd53ac73d

---
+++ manifests/develop/api.pp
@@ -0,0 +1,16 @@
+# Development class for designate API
+class designate::develop::api inherits designate::api {
+  Package['designate-api'] {
+    ensure=> absent,
+  }
+
+  file {'/etc/init/designate-api.conf':
+    source => 'puppet:///modules/designate/develop/api-init.conf',
+    before => Service['designate-api'],
+  }
+  file {'/usr/local/bin/designate-api':
+    ensure => link,
+    target => "/opt/${designate::openstack_version}/bin/designate-api",
+  }
+
+}
