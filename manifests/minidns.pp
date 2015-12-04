657cf3cfcb87c442829f3e031609babedcbda0c9
Andrew Spiers <andrew@andrewspiers.net>
2015-12-04 13:42:30 +1100

    Add minidns service
    Change-Id: Ia7e7d3a0b5d5a96f4cdd31842858ce7acf1cd86a

---
+++ manifests/minidns.pp
@@ -0,0 +1,22 @@
+# designate minidns
+class designate::minidns (
+  $workers = '1',
+  $host = '0.0.0.0',
+  $port = '5354',
+  $tcp_backlog = '100',
+  ) inherits profile::core::designate {
+
+  package {'designate-mdns':
+    ensure => 'installed',
+  }
+  service {'designate-mdns':
+    ensure => 'running',
+  }
+
+  designate_config {
+    'service:mdns/workers': value => $workers;
+    'service:mdns/host': value => $host;
+    'service:mdns/port': value => $port;
+    'service:mdns/tcp_backlog': value => $tcp_backlog;
+  }
+}
