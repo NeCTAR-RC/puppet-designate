fec2d0dc4cd4d58669bb66a3182de1cdb2c4901a
Andrew Spiers <andrew@andrewspiers.net>
2016-03-04 14:13:03 +1100

    Add manifest for designate-zone-manager
    Change-Id: I7233d129c6340100ebe310d05b9d934b12c51ed1

--- manifests/params.pp
+++ manifests/params.pp
@@ -13,6 +13,7 @@ class designate::params {
   $sink_service_name         = 'designate-sink'
   $mdns_service_name         = 'designate-mdns'
   $pool_manager_service_name = 'designate-pool-manager'
+  $zone_manager_service_name = 'designate-zone-manager'
 
   case $::osfamily {
     'RedHat': {
@@ -25,6 +26,7 @@ class designate::params {
       $pymysql_package_name      = undef
       $mdns_package_name         = 'openstack-designate-mdns'
       $pool_manager_package_name = 'openstack-designate-pool-manager'
+      $zone_manager_package_name = 'openstack-designate-zone-manager'
     }
     'Debian': {
       # package name
@@ -36,6 +38,7 @@ class designate::params {
       $pymysql_package_name      = 'python-pymysql'
       $pool_manager_package_name = 'designate-pool-manager'
       $mdns_package_name         = 'designate-mdns'
+      $zone_manager_package_name = 'designate-zone-manager'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")
