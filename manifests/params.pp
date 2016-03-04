78b63b90d4d8f84cf6f283e2f6e39c0079aed9eb
Andrew Spiers <andrew@andrewspiers.net>
2016-03-04 14:13:03 +1100

    Add manifest for designate-zone-manager
    Change-Id: I7233d129c6340100ebe310d05b9d934b12c51ed1

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,6 +9,8 @@ class designate::params {
   $log_dir                 =  '/var/log/designate'
   $client_package_name     =  'python-designateclient'
 
+  $zone_manager_service_name = 'designate-zone-manager'
+
   case $::osfamily {
     'RedHat': {
       # package name
@@ -19,6 +21,7 @@ class designate::params {
       $agent_package_name        = 'openstack-designate-agent'
       $pool_manager_package_name = 'openstack-designate-pool-manager'
       $sink_package_name         = 'openstack-designate-sink'
+      $zone_manager_package_name = 'openstack-designate-zone-manager'
       # service names
       $agent_service_name        = 'openstack-designate-agent'
       $api_service_name          = 'openstack-designate-api'
@@ -36,6 +39,7 @@ class designate::params {
       $agent_package_name        = 'designate-agent'
       $pool_manager_package_name = 'designate-pool-manager'
       $sink_package_name         = 'designate-sink'
+      $zone_manager_package_name = 'designate-zone-manager'
       # service names
       $agent_service_name        = 'designate-agent'
       $api_service_name          = 'designate-api'
---
+++ manifests/zone_manager.pp
@@ -0,0 +1,66 @@
+# == Class designate::zone_manager
+#
+# Configure designate zone manager service
+#
+# == Parameters
+#
+# [*package_ensure*]
+#  (optional) The state of the package
+#  Defaults to 'present'
+#
+# [*zone_manager_package_name*]
+#  (optional) Name of the package
+#  Defaults to zone_manager_package_name from ::designate::params
+#
+# [*enabled*]
+#   (optional) Whether to enable the service.
+#   Defaults to true
+#
+# [*service_ensure*]
+#  (optional) Whether the designate zone manager service will be running.
+#  Defaults to 'running'
+#
+# [*workers*]
+#  (optional) Number of workers to spawn.
+#  Defaults to 'None'
+#
+# [*threads*]
+#  (optional) Number of greenthreads to spawn
+#  Defaults to '1000'
+#
+# [*enabled_tasks*]
+#  (optional) List of tasks to enable, the default enables all tasks.
+#  tasks
+#  Defaults to 'None'
+#
+# [*export_synchronous*]
+#  (optional) Whether to allow synchronous zone exports
+#  Defaults to true
+#
+class designate::zone_manager (
+  $package_ensure            = 'present',
+  $package_name              = $::designate::params::zone_manager_package_name,
+  $enabled                   = true,
+  $service_ensure            = running,
+  $workers                   = $::os_service_default,
+  $threads                   = $::os_service_default,
+  $enabled_tasks             = $::os_service_default,
+  $export_synchronous        = $::os_service_default,
+  ) inherits designate {
+  include ::designate::params
+
+  designate_config {
+    'service:zone_manager/workers'            : value => $workers;
+    'service:zone_manager/threads'            : value => $threads;
+    'service:zone_manager/enabled_tasks'      : value => $enabled_tasks;
+    'service:zone_manager/export_synchronous' : value => $export_synchronous;
+  }
+
+  designate::generic_service { 'zone-manager':
+    ensure_package => $package_ensure,
+    enabled        => $enabled,
+    package_name   => $package_name,
+    manage_service => $service_ensure,
+    service_name   => $::designate::params::zone_manager_service_name,
+  }
+}
