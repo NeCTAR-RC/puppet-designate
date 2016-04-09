88b61ebc514b8cba2de21ee62e7856604d937655
Andrew Spiers <andrew@andrewspiers.net>
2016-04-10 09:41:52 +1000

    Include optional param also_notifies in pool.
    Pools with these cofigured send dns notifications to these additional
    name servers.
    
    Example configuration at
    https://github.com/openstack/designate/blob/stable/liberty/etc/designate/designate.conf.sample#L328
    
    Change-Id: Ieaa3d91aa76ea6c75f44559f3df6cdab98491497

--- manifests/pool.pp
+++ manifests/pool.pp
@@ -1,7 +1,19 @@
-define designate::pool ($targets, $nameservers) {
+# type for pool definitions.
+# Note: Will not work for Mitaka and future versions of designate.
+
+
+define designate::pool ($targets, $nameservers, $also_notifies = undef,) {
+
 
   designate_config {
     "pool:${name}/targets"     : value => $targets;
     "pool:${name}/nameservers" : value => $nameservers;
+  }
+
+  if $also_notifies {
+    validate_array($also_notifies)
+    designate_config { "pool:${name}/also_notifies": value  => join($also_notifies,',') }
+  } else {
+    designate_config { "pool:${name}/also_notifies": ensure => absent, }
   }
 }
